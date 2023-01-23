a9c1d5ade0672c0af93d1617e2336d106ae30966
CebollaFrit <121140015+CebollaFrit@users.noreply.github.com>
2023-01-23 07:09:04 -0500

    Add files via upload

--- corta-tus-frutas-plantilla-main/sketch.js
+++ corta-tus-frutas-plantilla-main/sketch.js
@@ -1,11 +1,11 @@
 //Game States
 var PLAY=1;
 var END=0;
-var gameState=1;
+var gameState= PLAY;
 
 var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
-var gameOverSound ,knifeSwoosh;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monster, monsterImage, gameOverImage;
+var gameOverSound ,knifeSwoosh, knifeSwooshSound;
 
 function preload(){
   
@@ -15,10 +15,10 @@ function preload(){
   fruit2 = loadImage("fruit2.png");
   fruit3 = loadImage("fruit3.png");
   fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png")
   
-  gameOverSound = loadSound("gameover.mp3")
-  knifeSwooshSound = loadSound("knifeSwoosh.mp3")
+  gameOverImage = loadImage("gameover.png");  
+  gameOverSound = loadSound("gameover.mp3"); 
+  knifeSwooshSound = loadSound("knifeSwoosh.mp3"); 
 }
 
 
@@ -29,17 +29,15 @@ function setup() {
   //crear cuchillo
    knife=createSprite(40,200,20,20);
    knife.addImage(knifeImage);
-   knife.scale=0.7
-  
-  
+   knife.scale=0.7;  
   
   //establecer el colisionador para el cuchillo
   knife.setCollider("rectangle",0,0,40,40);
 
   //variables Score y grupos
   score=0;
-  fruitGroup=createGroup();
-  monsterGroup=createGroup();
+  fruitGroup = createGroup();
+  monsterGroup = createGroup();
   
 }
 
@@ -47,41 +45,36 @@ function draw() {
   background("lightblue");
   
   if(gameState===PLAY){
-    
     //Llamar a las funciones fruits y Monster 
     fruits();
     Monster();
-    
+
     //Mover el cuchillo con el mouse
     knife.y=World.mouseY;
-    knife.x=World.mouseX;
+    knife.x=World.mouseX;    
   
     //Aumentar la puntuación si el cuchillo toca la fruta
     if(fruitGroup.isTouching(knife)){
+
       fruitGroup.destroyEach();
       
-       knifeSwooshSound.play();
-       knifeSwooshSound.play;
-       knifeSwooshSound();
-       knifeSwooshSoundplay();
+      knifeSwooshSound.play();
+      
+      score=score+2;   
 
-
-       score=score;
-       score=+2;
-       score=2;
-       score=score+2;
-
+    } else if(monsterGroup.isTouching(knife)){
+      gameState=END;
     }
-    else
-    {
-      //Cambiar a estado end si el cuchillo toca al enemigo
-      if(monsterGroup.isTouching(knife)){
-        gameState=END;
+
+
+  //Cambiar a estado end si el cuchillo toca al enemigo
+  } else if(gameState===END){
         //sonido de gameover
         gameOverSound.play()
         
         fruitGroup.destroyEach();
         monsterGroup.destroyEach();
+
         fruitGroup.setVelocityXEach(0);
         monsterGroup.setVelocityXEach(0);
         
@@ -91,18 +84,17 @@ function draw() {
         knife.x=300;
         knife.y=300;
       }
-    }
-  }
-  
-  drawSprites();
-  //Mostrar la puntuación
+    
+    
+    //Mostrar la puntuación
   textSize(25);
-  text("Puntuación : "+ score,250,50);
+  text("Puntuación : " + score,250,50);
+
+  drawSprites();
 }
 
-
 function Monster(){
-  if(World.frameCount%200===0){
+  if(frameCount% 200 === 0){
     monster=createSprite(400,200,20,20);
     monster.addAnimation("moving", monsterImage);
     monster.y=Math.round(random(100,550));
@@ -114,19 +106,45 @@ function Monster(){
 }
 
 function fruits(){
-  if(World.frameCount%80===0){
+  if(frameCount % 80 === 0){ 
+    
+    position = Math.round(random(1,2)); 
     fruit=createSprite(400,200,20,20);
-    fruit.x = 0    
+    
   //Aumentar la velocidad de la fruta después de que la puntuación llegue a 4 
 
-       fruit.velocityX= (7+(score/4));
-       fruit.velocityY= (7+(score));
-       fruit.velocity= (7+(score/4));
-       fruit.velocityX= (7);
+  if(position === 1){
+    fruit.x=600;
+    fruit.velocityX=-(7+(score/4));
+  } else{
+    fruit.x=0;
+    fruit.velocityX= (7+(score/4));
+  }   
+       
      
     fruit.scale=0.2;
-     fruit.debug=true;
-     r=Math.round(random(1,4));
+    fruit.debug=true;
+
+    var r = Math.round(random(1,4));
+    
+    switch(r){
+      case 1: 
+        fruit.addImage(fruit1);
+        break;
+      case 2:
+        fruit.addImage(fruit2);
+        break; 
+      case 3: 
+        fruit.addImage(fruit3);
+        break;
+      case 4:
+        fruit.addImage(fruit4);
+        break; 
+      default: 
+        break; 
+    }
+
+  /*
     if (r == 1) {
       fruit.addImage(fruit1);
     } else if (r == 2) {
@@ -135,7 +153,7 @@ function fruits(){
       fruit.addImage(fruit3);
     } else {
       fruit.addImage(fruit4);
-    }
+    }*/
     
     fruit.y=Math.round(random(50,550));
    
